5a91582c06c3bbd66f610a9440a199dad7a9b282
Siva Jagadeesan <sivajag@gmail.com>
2010-09-07 00:44:48 -0700

    first commit

---
+++ src/mql/core.clj
@@ -0,0 +1,1 @@
+(ns mql.core)
---
+++ test/mql/test/core.clj
@@ -0,0 +1,6 @@
+(ns mql.test.core
+  (:use [mql.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
