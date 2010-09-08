d7078d4942f352325f60267b80ee6caf5d3fab14
Siva Jagadeesan <sivajag@gmail.com>
2010-09-07 17:14:33 -0700

    added where clause with wildcard

--- src/mql/core.clj
+++ src/mql/core.clj
@@ -1,4 +1,41 @@
 (ns mql.core)
 
-(defn select [ks m]
-  (get-in m ks))
+(def ops {:gt >
+          :lt <
+          :ge >=
+          :le <=
+          :eq =})
+
+(defn select 
+  ([ks m]
+      (get-in m ks))
+  ([ks f m]
+     (f (select ks m))))
+
+(defn nks [m pks nk]
+  (if (= * nk)
+    (keys (get-in m pks))
+    (list nk)))
+
+(defn key-seq-copy [pks nks]
+  (apply concat (map (fn [pk] (map #(concat pk (vector %)) nks)) pks)))
+
+(defn key-seq [m kys]
+  (loop [ks [[]] k (first kys) rkys (rest kys)]
+    (if (nil? k)
+      ks
+      (recur (key-seq-copy ks (nks m (first ks) k)) (first rkys) (rest rkys)))))
+
+(defn valid-for-one? [mh key-seq ops-fn v]
+  (if (not= "$$NOT$$" (get-in mh key-seq "$$NOT$$"))
+    (ops-fn (read-string (get-in mh key-seq)) v)))
+
+(defn valid? [me key-seqs ops-fn v]
+  (let [mh (apply hash-map me)]
+    (some #(identity %) (map #(valid-for-one? mh % ops-fn v) key-seqs))))
+
+(defn where [ks o v]
+  (let [ops-fn (o ops)]
+    (fn [sm]
+      (let [key-seqs (key-seq sm ks)]
+        (filter (fn [me] (valid? me key-seqs ops-fn v)) sm)))))
---
+++ test/mql/test/core-test.clj
@@ -0,0 +1,62 @@
+(ns mql.test.core-test
+  (:use [mql.core] :reload)
+  (:use [clojure.test]))
+
+(def m
+     {:cid
+      {
+       :visits {
+                :id-1 {
+                       :last-ts "1274166000040"
+                       :first-ts "1274166000000"
+                       :duration "40"}
+                :id-2 {
+                       :last-ts "1274166000040"
+                       :first-ts "1274166000000"
+                       :duration "40"}
+                :id-3 {
+                       :last-ts "1274166000040"
+                       :first-ts "1274166000000"
+                       :duration "40"}}
+       
+       :promo{
+              :id-1 {:promo "p2"}
+              :id-2 {:promo "p1"}}
+       
+       :purchase {
+                  :id-1
+                  {:order-id "order-id-1"
+                   :total-dollars "970.00"
+                   :purchase? "true",
+                   :merchant-total-dollars "1000.00"}
+                  :id-3
+                  {:order-id "order-id-2"
+                   :total-dollars "1000.00"
+                   :purchase? ""
+                   :merchant-total-dollars "1000.00"}}}})
+
+(deftest test-key-seq-copy
+ (are [x y] (= x y)
+      '((:cid :promo :id-1) (:cid :promo :id-2)) (key-seq-copy [[:cid :promo]] [:id-1 :id-2])
+      '((:cid :promo1 :id) (:cid :promo2 :id)) (key-seq-copy [[:cid :promo1] [:cid :promo2]] [:id])))
+
+(deftest test-key-sequence
+  (are [x y] (= x y)
+       [[:cid :promo :id-1 :promo] [:cid :promo :id-2 :promo]] (key-seq m [:cid :promo * :promo])
+       ;[[:cid :purchase :id-1 :purchase?] [:cid :purchase :id-2 :purchase?]] (key-seq m [:cid * * :purchase?])
+       ))
+
+(deftest test-nks
+  (are [x y] (= x y)
+       '(:promo) (nks m [] :promo)
+       '(:id-1 :id-2) (nks m [:cid :promo] *)))
+
+
+(deftest test-simple-select
+  (is (= (select [:cid :promo] m) {:id-1 {:promo "p2"}
+                                   :id-2 {:promo "p1"}})))
+
+(deftest test-select-with-where
+  (is (= (keys (select [:cid :purchase] (where [* :total-dollars] :gt 980) m)) [:id-3])))
+
+
